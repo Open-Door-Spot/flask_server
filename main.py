9f68dbda307a5f1b47d5ee2a7dd3cd8dd9049615
rosellaliu <xuanru_liu@brown.edu>
2023-04-25 19:53:07 -0400

    client, server
    add some client server code for sending images between them

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 from flask import Flask, request
+from flask import jsonify, send_file, Response
+from PIL import Image
+import base64
 
 # command: python3 main.py
 from commands.hello_spot import hello_spot_main
@@ -18,5 +21,21 @@ def invoke_hello_spot():
 	os.system("arch -arm64 python3 commands/hello_spot.py "+SPOT_IP_ADDRESS)
 	return "its done"
 
+# put this here first and see if it works
+@app.route('/processing', methods=['POST'])
+def process():
+    file = request.files['image']
+    img = Image.open(file.stream)
+    img = base64.b64encode(img).decode() 
+    
+    return jsonify({
+                'msg': 'success', 
+                'size': [img.width, img.height], 
+                'format': img.format,
+                'img': img
+           })
+
 if __name__ == "__main__":
 	app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8888)))
+
+
